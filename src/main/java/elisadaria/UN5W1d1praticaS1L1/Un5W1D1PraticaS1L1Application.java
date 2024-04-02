a11dc45e75895c7e79bc7bf6741b38be86253742
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-04-02 15:31:07 +0200

    initialized-project-with-working-spring-boot

---
+++ src/main/java/elisadaria/UN5W1d1praticaS1L1/Un5W1D1PraticaS1L1Application.java
@@ -0,0 +1,14 @@
+package elisadaria.UN5W1d1praticaS1L1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Un5W1D1PraticaS1L1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Un5W1D1PraticaS1L1Application.class, args);
+		System.out.println("HEllo WORLD!");
+	}
+
+}
---
+++ src/test/java/elisadaria/UN5W1d1praticaS1L1/Un5W1D1PraticaS1L1ApplicationTests.java
@@ -0,0 +1,13 @@
+package elisadaria.UN5W1d1praticaS1L1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Un5W1D1PraticaS1L1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
